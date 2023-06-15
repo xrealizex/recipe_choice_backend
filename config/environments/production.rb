07860db261e7ab191a82728320c3f98e4c1a58fd
hiroto-hara <xre0605@gmail.com>
2023-06-15 22:09:14 +0900

    allow render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.hosts << "recipe-choice-backend.onrender.com"
 end
