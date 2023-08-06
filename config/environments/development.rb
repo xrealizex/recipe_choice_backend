a5fcd7ff63a95186c4fe604748b7f457a5f00f52
hiroto-hara <xre0605@gmail.com>
2023-08-06 18:01:44 +0900

    update dns url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,5 +62,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << "recipe-choice-backend.onrender.com"
+  config.hosts << "https://recipe-choice-backend-x552.onrender.com"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,5 +86,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts << "recipe-choice-backend.onrender.com"
+  config.hosts << "https://recipe-choice-backend-x552.onrender.com"
 end
