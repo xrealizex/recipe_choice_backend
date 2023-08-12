24d7f0bf3e9f3d7a3fb9f62a3936b2582f611243
hiroto-hara <xre0605@gmail.com>
2023-08-12 20:19:50 +0900

    update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "https://web.recipe-choice.shop"
     resource "*",
       headers: :any,
       expose: ["access-token", "expiry", "token-type", "uid", "client"],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      credentials: true
   end
 end
