e9ed527de92d1a8f429d88ad94d1f15192ea3790
hiroto-hara <xre0605@gmail.com>
2023-08-13 17:11:26 +0900

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://web.recipe-choice.shop"
+    origins "http://localhost:3000", "https://web.recipe-choice.shop", "https://recipe-choice.vercel.app"
     resource "*",
       headers: :any,
       expose: ["access-token", "expiry", "token-type", "uid", "client"],
